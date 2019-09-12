23b7e8038ffc6a4555bc77b85c79703ddd20d445
Roman Salo <frs.daddy@gmail.com>
2019-09-12 09:30:26 +0300

    Create the role_registration submodule
    Add a new select list with a list of form modes on the role edit page for the role_registration module

---
+++ modules/role_registration/src/Service/RoleRegistrationManager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\role_registration\Service;
+
+use Drupal\role_registration\Service\RoleRegistrationManagerInterface;
+
+/**
+ * Class RoleRegistrationManager.
+ */
+class RoleRegistrationManager implements RoleRegistrationManagerInterface {
+
+}
---
+++ modules/role_registration/src/Service/RoleRegistrationManagerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\role_registration\Service;
+
+use Drupal\user\UserInterface;
+
+/**
+ * Base interface definition for RoleRegistrationManagerInterface service.
+ */
+interface RoleRegistrationManagerInterface {
+
+  /**
+   * The module name.
+   */
+  const MODULE_NAME = 'role_registration';
+
+}
