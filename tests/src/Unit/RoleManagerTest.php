edd7eac06ac9ebb9a44766a2dab44be04ca1042a
Mariia Denysiuk <maria.denysyuk@gmail.com>
2019-09-16 14:39:50 +0300

    Update tests

--- tests/src/Unit/RoleManagerTest.php
+++ tests/src/Unit/RoleManagerTest.php
@@ -4,6 +4,8 @@ namespace Drupal\Tests\role\Unit;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\role\Plugin\RoleConfigElementManager;
 use Drupal\role\Service\RoleControlManager;
 use Drupal\Tests\UnitTestCase;
 
@@ -24,18 +26,63 @@ class RoleManagerTest extends UnitTestCase {
   protected $extraFields;
 
   /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface|
+   */
+  protected $moduleHandler;
+
+  /**
+   * Drupal\role\Plugin\RoleConfigElementManager definition.
+   *
+   * @var \Drupal\role\Plugin\RoleConfigElementManager
+   */
+  protected $roleConfigElementManager;
+
+  /**
+   * A list of role plugin definitions.
+   *
+   * @var array
+   */
+  protected $definitions = [
+    'account_form_mode' => [
+      'id' => 'account_form_mode',
+      'class' => 'Drupal\role\Plugin\Role\RoleConfigElement\AccountFormMode',
+    ],
+    'account_view_mode' => [
+      'id' => 'account_view_mode',
+      'class' => 'Drupal\role\Plugin\Role\RoleConfigElement\AccountViewMode',
+    ],
+  ];
+
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
     $container = new ContainerBuilder();
+
     $this->extraFields = ['account_form_mode', 'account_view_mode'];
+
     $entityTypeManager = $this->getMockBuilder(EntityTypeManagerInterface::class)
       ->disableOriginalConstructor()
       ->getMock();
-    $roleManager = new RoleControlManager($entityTypeManager);
-    \Drupal::setContainer($container);
+    $module_handler = $this->getMockBuilder(ModuleHandlerInterface::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $roleConfigElementManager = $this->getMockBuilder(RoleConfigElementManager::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $roleConfigElementManager->expects($this->any())
+      ->method('getDefinitions')
+      ->will($this->returnValue($this->definitions));
+
+    $roleManager = new RoleControlManager($entityTypeManager, $module_handler, $roleConfigElementManager);
+
     $container->set('role.control_manager', $roleManager);
+    \Drupal::setContainer($container);
+
   }
 
   /**
