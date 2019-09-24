f75a9c62667f518c119db1a23ef106f1c80dc0f7
Roman Salo <frs.daddy@gmail.com>
2019-09-24 17:57:52 +0300

    Remove unused method from the service

--- modules/role_registration/src/Service/RoleRegistrationManager.php
+++ modules/role_registration/src/Service/RoleRegistrationManager.php
@@ -3,7 +3,6 @@
 namespace Drupal\role_registration\Service;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\user\RoleInterface;
 
 /**
  * Class RoleRegistrationManager.
@@ -18,14 +17,4 @@ class RoleRegistrationManager implements RoleRegistrationManagerInterface {
     $form_state->setValue(['roles', $role_id], $role_id);
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getRegistrationThirdPartySettings(RoleInterface $role, $setting_name = NULL) {
-    $third_party_settings = $role->getThirdPartySettings(self::MODULE_NAME);
-    if ($setting_name) {
-      return $third_party_settings[$setting_name];
-    }
-    return $third_party_settings;
-  }
 }
